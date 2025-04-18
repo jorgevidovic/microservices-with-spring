542af4bd0f7a9e4019f07b2a5a61f9155ca8e423
Jorge I. Vidovic <jorgeisaacvidovich@gmail.com>
2025-04-18 23:48:04 +0200

    feat: Adding Student Entity

--- course-microservice/src/main/java/com/vsprojects/course/CourseMicroserviceApplication.java
+++ course-microservice/src/main/java/com/vsprojects/course/CourseMicroserviceApplication.java
@@ -1,4 +1,4 @@
-package com.vsprojects.student;
+package com.vsprojects.course;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-public class StudentMicroserviceApplication {
+public class CourseMicroserviceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StudentMicroserviceApplication.class, args);
+		SpringApplication.run(CourseMicroserviceApplication.class, args);
 	}
 
 }
---
+++ course-microservice/src/test/java/com/vsprojects/course/CourseMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vsprojects.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
